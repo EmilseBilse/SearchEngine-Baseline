746076c69647861f1c2b98fc4666b2c9cdc06fbe
EmilseBilse <bandittenxd@gmail.com>
2023-03-01 21:15:44 +0100

    3 videos

--- Common/Config.cs
+++ Common/Config.cs
@@ -2,8 +2,8 @@ namespace Common
 {
     public static class Config
     {
-        public static string DatabasePath { get; } = "/Users/tbmh/DLS/database.db";
-        public static string DataSourcePath { get; } = "/Users/tbmh/DLS/source";
+        public static string DatabasePath { get; } = "D:/work/pbsw/large-systems/database.db";
+        public static string DataSourcePath { get; } = "D:/work/pbsw/large-systems/source/maildir";
         public static int NumberOfFoldersToIndex { get; } = 10; // Use 0 or less for indexing all folders
     }
 }
