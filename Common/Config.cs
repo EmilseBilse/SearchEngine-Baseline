c2f1ef1bbd0c7cab6d14732f8b60498a8b3c20f8
EmilseBilse <71692416+EmilseBilse@users.noreply.github.com>
2023-03-09 10:58:35 +0100

    save

--- Common/Config.cs
+++ Common/Config.cs
@@ -2,8 +2,8 @@ namespace Common
 {
     public static class Config
     {
-        public static string DatabasePath { get; } = "D:/work/pbsw/large-systems/database.db";
-        public static string DataSourcePath { get; } = "D:/work/pbsw/large-systems/source/maildir";
+        public static string DatabasePath { get; } = "/data/database.db";
+        public static string DataSourcePath { get; } = "/data/EnronMini";
         public static int NumberOfFoldersToIndex { get; } = 10; // Use 0 or less for indexing all folders
     }
 }
--- WebSearch/wwwroot/js/site.js
+++ WebSearch/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let ViewModel = function () {
+
+    let me = this;
+    
+    me.searchTerms = ko.observable();
+    me.hits = ko.observable();
+    me.results = ko.observableArray();
+    me.timeUsed = ko.observable();
+    
+    me.search = function() {
+        $.ajax({
+            url: "http://localhost:9000/Search?terms=" + me.searchTerms() + "&numberOfResults=10",
+            success: function(data) {
+                data = JSON.parse(data);
+                console.log(data);
+                me.hits(data.Hits);
+                me.timeUsed(data.TimeUsed);
+                me.results.removeAll();
+                data.DocumentHits.forEach(function(hit) {
+                    me.results.push(hit);
+                });
+                console.log(me.hits());
+                console.log(me.timeUsed());
+            }
+        });
+    }
+
+};
+ko.applyBindings(new ViewModel());
